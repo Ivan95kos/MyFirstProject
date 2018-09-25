3989a17de0c6e6fee5e77bab5724cc19dd0308ab
ivan <ivan95kos@gmail.com>
2018-09-25 12:21:06 +0300

    add Exception for file

--- src/main/java/com/example/MyFirstProject/exception/CustomException.java
+++ src/main/java/com/example/MyFirstProject/exception/CustomException.java
@@ -2,14 +2,14 @@ package com.example.MyFirstProject.exception;
 
 import org.springframework.http.HttpStatus;
 
-
 public class CustomException extends RuntimeException {
+
     private static final long serialVersionUID = 1L;
 
     private final String message;
     private final HttpStatus httpStatus;
 
-    public CustomException(String message, HttpStatus httpStatus) {
+    public CustomException(final String message, final HttpStatus httpStatus) {
         this.message = message;
         this.httpStatus = httpStatus;
     }
