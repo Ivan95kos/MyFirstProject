5f447b6b7a96a5926f918b0fc0d38d36a6c35a2d
Ivan Kosovych <ivan95kos@gmail.com>
2018-11-02 10:04:43 +0200

    Purging Expired Tokens Generated By The Reset Password

---
+++ src/main/java/com/example/MyFirstProject/config/SpringTaskConfig.java
@@ -0,0 +1,11 @@
+package com.example.MyFirstProject.config;
+
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@NoArgsConstructor
+public class SpringTaskConfig {
+}
