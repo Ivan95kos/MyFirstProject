8a89dc21fdbfc21332550067a4002782291e8e4a
Ivan Kosovych <ivan95kos@gmail.com>
2018-11-15 11:10:22 +0200

    update

--- src/main/java/com/example/MyFirstProject/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/MyFirstProject/service/UserDetailsServiceImpl.java
@@ -2,12 +2,14 @@ package com.example.MyFirstProject.service;
 
 import com.example.MyFirstProject.model.User;
 import com.example.MyFirstProject.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
@@ -17,16 +19,19 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
         User user = userRepository.findOneByUsernameOrEmail(usernameOrEmail, usernameOrEmail);
-//        Hibernate.initialize(user.getPassword());
-//        System.out.println(user.getPassword());
+
+//        Hibernate.initialize(user.getUsername());
+//        System.out.println(user.getUsername());
+
         if (user == null) {
+            log.info("User with name: " + usernameOrEmail + " not found");
             throw new UsernameNotFoundException("User with name: " + usernameOrEmail + " not found");
         }
 
 //        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), true, true, true, true,
 //                new ArrayList<>());
         return org.springframework.security.core.userdetails.User//
-                .withUsername(usernameOrEmail)//
+                .withUsername(user.getUsername())//
                 .password(user.getPassword())//
                 .authorities(user.getRoles())//
                 .accountExpired(false)//
