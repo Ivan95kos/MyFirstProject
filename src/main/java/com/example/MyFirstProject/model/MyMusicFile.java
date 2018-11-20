7b0e603daf30bccc47207a361ce98b2f77eca891
Ivan Kosovych <ivan95kos@gmail.com>
2018-11-20 16:16:17 +0200

    Add new Field for MyMusicFile

--- src/main/java/com/example/MyFirstProject/model/MyMusicFile.java
+++ src/main/java/com/example/MyFirstProject/model/MyMusicFile.java
@@ -1,6 +1,7 @@
 package com.example.MyFirstProject.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.mpatric.mp3agic.Mp3File;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,14 +14,23 @@ import javax.persistence.*;
 @NoArgsConstructor
 @ToString
 @Entity
-public class MyFile {
+public class MyMusicFile {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
+    private String fileName;
+
+    @Column
     private String localAddress;
+
+    @Column
+    private String contentType;
+
+    @Transient
+    private Mp3File mp3File;
 
     @JsonBackReference
     @ManyToOne
